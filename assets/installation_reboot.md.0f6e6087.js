492b75dd8a7cfe0e494493bd59f019e48fd3c35e
D3SOX <D3SOX@users.noreply.github.com>
2022-06-07 13:05:23 +0000

    Merge pull request #64 from D3SOX/dependabot/github_actions/actions/setup-node-3
    build(deps): bump actions/setup-node from 2 to 3 d429df35b8306556f8386cade384e7a639e7b16f

--- assets/installation_reboot.md.0f6e6087.js
+++
@@ -1,4 +0,0 @@
-import{_ as a,c as s,o as t,a as e}from"./app.962f199f.js";const C='{"title":"Reboot","description":"","frontmatter":{},"headers":[],"relativePath":"installation/reboot.md","lastUpdated":1654607006000}',n={name:"installation/reboot.md"},o=e(`<h1 id="reboot" tabindex="-1">Reboot <a class="header-anchor" href="#reboot" aria-hidden="true">#</a></h1><div class="language-"><span class="copy"></span><pre><code><span class="line"><span style="color:#A6ACCD;">exit</span></span>
-<span class="line"><span style="color:#A6ACCD;">umount -R /mnt</span></span>
-<span class="line"><span style="color:#A6ACCD;">reboot</span></span>
-<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div><p>Continue with <a href="/post-installation/">Post-Installation</a></p>`,3),l=[o];function r(p,c,i,_,d,h){return t(),s("div",null,l)}var m=a(n,[["render",r]]);export{C as __pageData,m as default};
--- assets/installation_reboot.md.0f6e6087.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as s,o as t,a as e}from"./app.962f199f.js";const C='{"title":"Reboot","description":"","frontmatter":{},"headers":[],"relativePath":"installation/reboot.md","lastUpdated":1654607006000}',n={name:"installation/reboot.md"},o=e("",3),l=[o];function r(p,c,i,_,d,h){return t(),s("div",null,l)}var m=a(n,[["render",r]]);export{C as __pageData,m as default};
