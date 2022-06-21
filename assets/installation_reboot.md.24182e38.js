fb723581de6ef352916d458ccc22fc5ee2469561
D3SOX <D3SOX@users.noreply.github.com>
2022-06-21 13:22:29 +0000

    fix: add more languages to code blocks 4c97d2fed1870d8fa1125dde8fa86412ffcb7507

---
+++ assets/installation_reboot.md.24182e38.js
@@ -0,0 +1,4 @@
+import{_ as a,c as s,o as t,a as e}from"./app.f61b5596.js";const m=JSON.parse('{"title":"Reboot","description":"","frontmatter":{},"headers":[],"relativePath":"installation/reboot.md","lastUpdated":1655817715000}'),o={name:"installation/reboot.md"},n=e(`<h1 id="reboot" tabindex="-1">Reboot <a class="header-anchor" href="#reboot" aria-hidden="true">#</a></h1><div class="language-bash"><span class="copy"></span><pre><code><span class="line"><span style="color:#82AAFF;">exit</span></span>
+<span class="line"><span style="color:#A6ACCD;">umount -R /mnt</span></span>
+<span class="line"><span style="color:#A6ACCD;">reboot</span></span>
+<span class="line"></span></code></pre></div><div class="tip custom-block"><p class="custom-block-title">TIP</p><p>Now continue with <a href="/post-installation/">Post-Installation</a></p></div>`,3),l=[n];function c(p,r,i,_,d,h){return t(),s("div",null,l)}var u=a(o,[["render",c]]);export{m as __pageData,u as default};
