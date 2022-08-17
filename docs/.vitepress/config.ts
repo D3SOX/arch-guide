73fc20332b985843c19f665fea3c95ddd0718571
D3SOX <nico@d3sox.me>
2022-08-17 13:17:05 +0200

    fix: add workaround for footer undefined issue
    https://github.com/vuejs/vitepress/issues/1190

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -66,6 +66,7 @@ export default defineConfig({
 
         footer: {
             message: 'Made by D3SOX with ❤️',
+            copyright: '' // workaround for https://github.com/vuejs/vitepress/issues/1190
         },
 
         algolia: {
