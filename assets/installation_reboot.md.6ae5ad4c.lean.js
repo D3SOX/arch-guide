1f995fd64941620e4aaed330e2b042b645e5d991
D3SOX <D3SOX@users.noreply.github.com>
2022-08-17 10:47:40 +0000

    fix(deps): bump vitepress to 1.0.0-alpha.8 4b650b160ecfe8447fb4aef541640a020a94772e

--- assets/installation_reboot.md.6ae5ad4c.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as s,o as t,a as o}from"./app.03dfbdd7.js";const u=JSON.parse('{"title":"Reboot","description":"","frontmatter":{},"headers":[],"relativePath":"installation/reboot.md","lastUpdated":1660624126000}'),e={name:"installation/reboot.md"},n=o("",3),l=[n];function c(p,r,i,_,d,b){return t(),s("div",null,l)}var m=a(e,[["render",c]]);export{u as __pageData,m as default};
