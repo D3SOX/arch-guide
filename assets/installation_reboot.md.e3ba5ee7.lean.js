a186afad7f61ab52f2cd084ed338246f15e4e6ed
D3SOX <D3SOX@users.noreply.github.com>
2022-11-15 13:23:33 +0000

    Merge pull request #89 from D3SOX/dependabot/npm_and_yarn/vitepress-1.0.0-alpha.29
    build(deps-dev): bump vitepress from 1.0.0-alpha.28 to 1.0.0-alpha.29 6e6ce2a4b1447a3ddf4fcd6755609c1cdc7d6be7

---
+++ assets/installation_reboot.md.e3ba5ee7.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as s,o as t,a as o}from"./app.7dec25d4.js";const u=JSON.parse('{"title":"Reboot","description":"","frontmatter":{},"headers":[],"relativePath":"installation/reboot.md","lastUpdated":1668518575000}'),e={name:"installation/reboot.md"},n=o("",3),l=[n];function c(i,p,r,_,d,h){return t(),s("div",null,l)}const m=a(e,[["render",c]]);export{u as __pageData,m as default};
